57b9be83cd667b7ca12fb5a4907b6a0d83484be5
d0decahedron <54024825+d0decahedron@users.noreply.github.com>
2021-04-27 11:17:03 +0200

    Zipf's Law
    (still needs the "true Zipfian dist" and the MSE, will do that later)

--- assignments/assignment-I/exercise_3.py
+++ assignments/assignment-I/exercise_3.py
@@ -1,3 +1,8 @@
+# QUESTION: Can we use external packages? (i presume we can since they also used some lol)
+from nltk import FreqDist
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import loglog
+
 def analysis(name, data):
   """
   Plot Zipfian distribution of words + true Zipfian distribution. Compute MSE.
@@ -5,4 +10,17 @@ def analysis(name, data):
   :param name: title of the graph
   :param data: list of words
   """
-  print("TODO", name)
+  # NOTE: just Zipfian dist, needs "true Zipfian dist" + MSE
+  
+  freq = FreqDist(data)                # freq - frequency distribution
+	sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)   # sorted_freq - frequency distribution sorted by values in descending order
+	freq_values = [i[1] for i in sorted_freq]
+
+	x = [i for i in range(1, (len(freq_values) + 1))]   # x - sequence of numbers from 1 to the # of tokens  
+  
+  plt.loglog(x, freq_values, "bo")
+	plt.title("Log plot - {}".format(name))
+	plt.show()
+  
+  
+#   print("TODO", name)
