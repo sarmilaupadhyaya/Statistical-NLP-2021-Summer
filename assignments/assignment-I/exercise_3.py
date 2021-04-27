5e889d0bafcf5196b4cb30c0900575e6f5589bc0
d0decahedron <54024825+d0decahedron@users.noreply.github.com>
2021-04-27 12:36:09 +0200

    Fixed indents, oops

--- assignments/assignment-I/exercise_3.py
+++ assignments/assignment-I/exercise_3.py
@@ -1,26 +1,28 @@
 # QUESTION: Can we use external packages? (i presume we can since they also used some lol)
+
 from nltk import FreqDist
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import loglog
 
 def analysis(name, data):
-  """
-  Plot Zipfian distribution of words + true Zipfian distribution. Compute MSE.
+	
+	""" 
+	 Plot Zipfian distribution of words + true Zipfian distribution. Compute MSE.
 
-  :param name: title of the graph
-  :param data: list of words
-  """
-  # NOTE: just Zipfian dist, needs "true Zipfian dist" + MSE
+  	:param name: title of the graph
+  	:param data: list of words
   
-  freq = FreqDist(data)                # freq - frequency distribution
+  	"""
+	
+# 	  NOTE: just Zipfian dist, needs "true Zipfian dist" + MSE
+
+	freq = FreqDist(data)                # freq - frequency distribution
 	sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)   # sorted_freq - frequency distribution sorted by values in descending order
 	freq_values = [i[1] for i in sorted_freq]
-
 	x = [i for i in range(1, (len(freq_values) + 1))]   # x - sequence of numbers from 1 to the # of tokens  
-  
-  plt.loglog(x, freq_values, "bo")
+	plt.loglog(x, freq_values, "bo")
 	plt.title("Log plot - {}".format(name))
 	plt.show()
-  
+
   
 #   print("TODO", name)
