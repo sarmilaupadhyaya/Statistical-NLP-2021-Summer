77a1686f5e1952a88f09ded3c34962549ebd1c52
Sharmila Upadhyaya <sarmilaupadhyaya@outlook.com>
2021-04-29 19:40:45 +0200

    changes the tab to 4 white space

--- assignments/assignment-I/exercise_3.py
+++ assignments/assignment-I/exercise_3.py
@@ -3,23 +3,23 @@ from matplotlib.pyplot import loglog
 import numpy as np
 
 def analysis(name, data):
-	
-		
-	""" 
-	 Plot Zipfian distribution of words + true Zipfian distribution. Compute MSE.
+    
+        
+    """ 
+     Plot Zipfian distribution of words + true Zipfian distribution. Compute MSE.
 
-  	:param name: title of the graph
-  	:param data: list of words
+      :param name: title of the graph
+      :param data: list of words
   
-  	"""
+      """
 
     counts = [(word, data.count(word) / len(data)) for word in set(data)] 
     sorted_freq = sorted(counts, key = lambda x: x[1], reverse=True)
     rank = range(1,len(sorted_freq)+1)
     freq_list = [tup[1] for tup in sorted_freq]
-	
+    
     ideal = [sorted_freq[0][1]/r for r in rank] # True Zipfian distribution
-	
+    
     mse = ((np.array(ideal) - np.array(freq_list))**2).mean() # MSE
 
 
@@ -29,4 +29,4 @@ def analysis(name, data):
     plt.xlabel("Rank")
     plt.ylabel("Frequency")
     plt.show()
-	print(f"MSE, {name} = {mse}")
+    print(f"MSE, {name} = {mse}")
