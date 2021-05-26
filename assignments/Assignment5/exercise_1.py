860b8298c4557de35de24e10e1a3801d88ab656e
dors <jeknic@protonmail.com>
2021-05-27 00:25:54 +0200

    added preprocess and train/test split functions

--- assignments/Assignment5/exercise_1.py
+++ assignments/Assignment5/exercise_1.py
@@ -1,12 +1,33 @@
 from typing import List, Dict
+import string
 #Add imports
 
 def preprocess(text) -> List:
-    return []
+
+    clean = []
+
+    # remove punctuation
+
+    for char in text.lower():
+        if char in string.punctuation:
+            pass
+        else:
+            clean.append(char)
+    new = "".join(clean)
+
+    tokens = new.split()
+
+    return tokens
 
 
 def train_test_split_data(text:List, test_size:float=0.1):
-    return []
+
+    train_split = int(len(text) * test_size)
+    train = text[:train_split]
+    test = text[train_split:]
+
+
+    return train, test
 
 
 def get_oov_rates(train:List, test:List) -> List:
