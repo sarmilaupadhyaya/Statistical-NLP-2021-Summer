5c36ff3c7e65eae8b27e87999be69376766617d5
Sharmila Upadhyaya <sarmilaupadhyaya@outlook.com>
2021-05-28 15:59:44 +0200

    added plots:

--- assignments/Assignment5/exercise_2.py
+++ assignments/Assignment5/exercise_2.py
@@ -1,14 +1,21 @@
 from typing import Dict, List
+import matplotlib.pyplot as plt
 
 def plot_pp(pps: Dict):
     """ Plots perplexity vs n for all languages in the corpus
 
     :param pps: dictionary with langs as keys and lists of perplexity scores as values
     """
+    for j in range(0, len(list(pps.values())[0])):
+        color = ['red', 'blue', 'green', 'pink']
+        plt.figure(figsize=(10, 10), dpi=80)
+        for i, (lang, pp) in enumerate(pps.items()): 
+            lables = ['unigram']
+            plt.plot(lables, [pp[j]], 'o', color=color[i])
+        plt.legend(pps.keys())
+        plt.title("Perplexity for " + str(j+1) + "gram model")
+        plt.show()
 
-    lables = ['unigram', 'bigram', 'trigram']
-    plt.plot(lables, pps, 'bo')
-    plt.show()
 
 
 
@@ -17,3 +24,6 @@ def plot_pp_vs_alpha(pps: List[float], alphas: List[float]):
     :param pps: list of perplexity scores
     :param alphas: list of alphas
     """
+    plt.figure(figsize=(10, 10), dpi=80)
+    plt.plot(alphas,pps)
+    plt.show()
