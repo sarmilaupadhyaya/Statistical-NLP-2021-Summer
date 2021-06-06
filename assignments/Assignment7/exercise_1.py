9cc610db775d1e923ac7584fa9b625b38373e44c
Sharmila Upadhyaya <sarmilaupadhyaya@outlook.com>
2021-06-06 13:19:59 +0200

    initiate

---
+++ assignments/Assignment7/exercise_1.py
@@ -0,0 +1,19 @@
+
+from collections import defaultdict
+import math
+
+class CountTree():
+  def __init__(self, n=4):
+    pass
+  
+  def add(self, ngram):
+    pass
+
+  def get(self, ngram):
+    pass
+
+  def perplexity(self, ngrams, vocab):
+    pass
+
+  def prune(self, k):
+    pass
